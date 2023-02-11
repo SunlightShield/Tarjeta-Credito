f784f8fbb0176118cf2f5745e84a03a0382a2d55
sebastian <sespinozahenriquez@gmail.com>
2023-02-10 23:41:22 -0300

    proyecto base

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+
+
+<div className="card border-light mb-3">
+  <div className="card-header text-dark">
+    Tarjeta de credito
+  </div>
+  <div className="card-body">
+    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
